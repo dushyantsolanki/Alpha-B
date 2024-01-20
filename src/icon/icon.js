f02ec93c35941b73bd2d0b77046376845f08ff16
dushyantsolanki <dushyantsolanki.2002@gmail.com>
2024-01-20 21:28:56 +0530

    create a icon file for use a icon in whole application

---
+++ src/icon/icon.js
@@ -0,0 +1,25 @@
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { CloudUploadRounded } from "@mui/icons-material";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import MailIcon from "@mui/icons-material/Mail";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import GoogleIcon from "@mui/icons-material/Google";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/FaceBook";
+
+export {
+  IconButton,
+  VisibilityIcon,
+  VisibilityOffIcon,
+  MailIcon,
+  AccountCircleIcon,
+  GoogleIcon,
+  TwitterIcon,
+  FacebookIcon,
+  ChevronLeftIcon,
+  MenuIcon,
+  CloudUploadRounded,
+};
