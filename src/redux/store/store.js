af31191ec11cae403dc535277f8691d15d7f8696
dushyantsolanki <dushyantsolanki.2002@gmail.com>
2024-01-20 21:31:33 +0530

    Create : store for dedux

---
+++ src/redux/store/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import authReducer from "../reducers/authReducer";
+
+const rootReducer = combineReducers({
+  authReducer: authReducer,
+
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+export default store;
