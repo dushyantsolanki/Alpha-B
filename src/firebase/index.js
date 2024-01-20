df61f15dbc9326e877e31e3a8bd1df3f787a5a37
dushyantsolanki <dushyantsolanki.2002@gmail.com>
2024-01-20 21:26:08 +0530

    craete a index file for firebase folder

---
+++ src/firebase/index.js
@@ -0,0 +1,5 @@
+import { authentication } from "./authentication/authentication";
+import { bucket } from "./storage/storage";
+import { firestore } from "./firestore/firestore";
+
+export {authentication,bucket,firestore}
