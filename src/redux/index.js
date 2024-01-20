2de7fdba1430bda08f19244b86dae76c3fe5ab58
dushyantsolanki <dushyantsolanki.2002@gmail.com>
2024-01-21 00:58:51 +0530

    currentUser and logout actions import and export

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,4 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
 import store from "./store/store";
+import { currentUser, logout } from "./reducers/authReducer";
 
-export { store, useSelector, useDispatch };
+export { store, useSelector, useDispatch, currentUser, logout };
