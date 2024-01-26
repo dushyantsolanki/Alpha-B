76b0294792c3ff5fef5ae100d50bc8959a96e55c
Dushyant Solanki <112596904+dushyantsolanki@users.noreply.github.com>
2024-01-26 18:04:21 +0000

    facebook icon update

--- src/icon/icon.js
+++ src/icon/icon.js
@@ -8,7 +8,7 @@ import MailIcon from "@mui/icons-material/Mail";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import GoogleIcon from "@mui/icons-material/Google";
 import TwitterIcon from "@mui/icons-material/Twitter";
-import FacebookIcon from "@mui/icons-material/FaceBook";
+import FacebookIcon from "@mui/icons-material/Facebook";
 
 export {
   IconButton,
