c443780deecdf60fe79eff6131c577da7bdd3151
dushyantsolanki <dushyantsolanki.2002@gmail.com>
2024-01-26 23:10:04 +0530

    Import and Export a Profile Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,4 +4,5 @@ import ErrorPage from "./404 page/ErrorPage";
 import Dashboard from "./dashboard/Dashboard";
 import Editor from "./editor/Editor";
 import ForgotPassword from "./forgot-password/ForgotPassword.jsx";
-export { Login, Signup, Dashboard, ErrorPage, Editor, ForgotPassword };
+import Profile from "./profile/Profile";
+export { Login, Signup, Dashboard, ErrorPage, Editor, ForgotPassword, Profile };
