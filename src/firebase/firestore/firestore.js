00c68b182be0946e6dd63d021445d15a18692dc1
dushyantsolanki <dushyantsolanki.2002@gmail.com>
2024-01-26 23:04:56 +0530

    Add a update a existing document in firestore

--- src/firebase/firestore/firestore.js
+++ src/firebase/firestore/firestore.js
@@ -3,17 +3,27 @@ import {
   getFirestore,
   collection,
   addDoc,
+  setDoc,
   getDocs,
   query,
   where,
+  doc,
 } from "firebase/firestore";
+import {
+  getAuth,
+  updateProfile,
+  updateEmail,
+  updatePassword,
+} from "firebase/auth";
 import { config } from "../config/config";
 
 class Firebase_Firestore {
   app = initializeApp(config);
   db;
+  auth;
   constructor() {
     this.db = getFirestore(this.app);
+    this.auth = getAuth(this.app);
   }
 
   sendDoc = async ({
@@ -112,6 +122,72 @@ class Firebase_Firestore {
     } catch (error) {
       console.log(
         `firebase : firestore : firestore.js : retriveDoc : error_message => ${error.message}`
+      );
+
+      return { code: error?.code, message: error?.message, status: false };
+    }
+  };
+
+  retriveFirestoreDocRegister = async (userId) => {
+    try {
+      const usersCollection = collection(this.db, "register");
+
+      const snapshot = await getDocs(
+        query(usersCollection, where("userId", "==", userId))
+      );
+      const documents = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      return {
+        code: "success",
+        message: "register was fetch successfully",
+        payload: documents,
+        status: true,
+      };
+    } catch (error) {
+      console.log(
+        `firebase : firestore : firestore.js : retriveFirestoreDocRegister : error_message => ${error.message}`
+      );
+
+      return { code: error?.code, message: error?.message, status: false };
+    }
+  };
+
+  updateRegisterDoc = async (
+    { userName, firstName, lastName, imageUrl },
+    uid,
+    id,
+    userImage,
+    email,
+    password
+  ) => {
+    try {
+      const docRef = await setDoc(doc(this.db, "register", id), {
+        userName,
+        firstName,
+        lastName,
+        email: email,
+        password: password,
+        userImage: imageUrl || userImage,
+        userId: uid,
+      });
+
+      // Firebase authentication service profile update
+      updateProfile(this.auth.currentUser, {
+        displayName: userName,
+        photoURL: imageUrl || userImage,
+      });
+
+      return {
+        code: "success",
+        message: "Data was updated successfully",
+        payload: docRef,
+        status: true,
+      };
+    } catch (error) {
+      console.log(
+        `firebase : firestore : firestore.js : updateRegisterDoc : error_message => ${error.message}`
       );
 
       return { code: error?.code, message: error?.message, status: false };
