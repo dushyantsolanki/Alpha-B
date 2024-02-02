a8c2b72fc62feb5f32c8d1f10c5bbc69195e388a
dushyantsolanki <fullmoj70@gmail.com>
2024-02-03 02:01:06 +0530

    add : Blogs component

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,4 +1,5 @@
 import AuthLayout from "./authLayout/AuthLayout";
 import DashboardLayout from "./dashboardLayout/DashboardLayout";
+import HomeLayout from "./homeLayout/HomeLayout";
 
-export { AuthLayout, DashboardLayout };
+export { AuthLayout, DashboardLayout, HomeLayout };
