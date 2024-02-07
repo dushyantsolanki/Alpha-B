cbab93e0d4787f1797acd7af3d9b7fbb2eb3039f
dushyantsolanki <fullmoj70@gmail.com>
2024-02-07 16:57:57 +0530

    Home Layout created

--- src/layout/homeLayout/HomeLayout.jsx
+++ src/layout/homeLayout/HomeLayout.jsx
@@ -1,10 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { SwiperCompo } from "../../pages";
 
 function HomeLayout() {
+  const navigate = useNavigate();
   return (
     <>
-      
-      <Outlet />
+      <div className="home-layout">
+        <div className="swiper">
+          <SwiperCompo />
+        </div>
+        <button
+          onClick={() => {
+            navigate("/dashboard/editor");
+          }}
+        >
+          {" "}
+          Go to Editor
+        </button>
+        <div className="item-blogs">
+          <Outlet />
+        </div>
+      </div>
     </>
   );
 }
