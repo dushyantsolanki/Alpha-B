f8169c0e307c5c496a766f7dd49670ef7a6b71ba
dushyantsolanki <fullmoj70@gmail.com>
2024-07-11 18:28:08 +0530

    change name Items to items

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,8 @@ import Editor from "./editor/Editor";
 import ForgotPassword from "./forgot-password/ForgotPassword";
 import Profile from "./profile/Profile";
 import Items from "./items/Items";
-import FullItem from "./Items/FullItem";
+
+import FullItem from "./items/FullItem.jsx";
 import Home from "./home/Home";
 import Cart from "./cart page/Cart";
 import Contact from "./contact page/Contact";
